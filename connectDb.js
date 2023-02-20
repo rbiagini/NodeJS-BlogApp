8dd5c1ee4be6c27218b99cc4cfda3f810d00aa9f
Ronaldo Biagini <rbiagini@gmail.com>
2023-02-20 19:06:58 -0300

    inicial commit

--- connectDb.js
+++ connectDb.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 const connectToDatabase = async () => {
-    console.log(process.env.NODE_ENV);
-    if (process.env.NODE_ENV == "production") {
+    if (1 == 1 || process.env.NODE_ENV == "production") {
         var MONGODB_USERNAME = "biagini";
         var MONGODB_PASSWORD = "2fsuA6BL82gwX37N";
         var MONGODB_SERVER = "nodejs.xqcrckt.mongodb.net/BlogApp";
@@ -18,7 +17,7 @@ const connectToDatabase = async () => {
     const connectString = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_SERVER}?retryWrites=true&w=majority`;
 
     //conexão original
-    await mongoose.connect(connectString, (err) => {
+    const db = mongoose.connect(connectString, (err) => {
         if (err) return console.log("Ocorreu um erro de conexão\n\n", err);
 
         return console.log("conexão bem sucedida com mongodb.net");
